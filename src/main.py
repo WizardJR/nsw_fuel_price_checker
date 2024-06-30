155c4c662dafe1f5186030957e0af3d29cabe4d8
calvin360 <61894408+calvin360@users.noreply.github.com>
2024-06-30 20:29:24 +1000

    test commit

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
+import json
 import requests
 from datetime import datetime
-import pytz
+# import pytz
 
 url = "https://api.onegov.nsw.gov.au/oauth/client_credential/accesstoken?grant_type=client_credentials"
 
@@ -8,7 +9,7 @@ headers = {
     'grant_type': "client_credentials",
     'accept': "application/json",
     'Authorization': "Basic MU1ZU1JBeDV5dnFIVVpjNlZHdHhpeDZvTUEycWdmUlQ6Qk12V2FjdzE1RXQ4dUZHRg==",
-    }
+}
 
 response = requests.request("GET", url, headers=headers)
 
@@ -30,11 +31,25 @@ headers = {
     'apikey': "1MYSRAx5yvqHUZc6VGtxix6oMA2qgfRT",
     'transactionid': "123123",
     'requesttimestamp': dt.strftime("%d-%m-%y %I:%M:%S %p"),
-    }
+}
 response = requests.request("GET", url, headers=headers)
 
 # print(response.json())
-import json
 with open('data.json', 'w') as f:
     json.dump(response.json(), f)
     f.close()
+
+url = "https://api.onegov.nsw.gov.au/FuelPriceCheck/v1/fuel/prices/location"
+
+payload = "{\"fueltype\":\"\",\"brand\":[],\"namedlocation\":\"\",\"referencepoint\":{\"latitude\":\"\",\"longitude\":\"\"},\"sortby\":\"\",\"sortascending\":\"\"}"
+headers = {
+    'content-type': "<SOME_STRING_VALUE>",
+    'authorization': "<SOME_STRING_VALUE>",
+    'apikey': "<SOME_STRING_VALUE>",
+    'transactionid': "<SOME_STRING_VALUE>",
+    'requesttimestamp': "<SOME_STRING_VALUE>"
+}
+
+response = requests.request("POST", url, data=payload, headers=headers)
+
+print(response.text)
