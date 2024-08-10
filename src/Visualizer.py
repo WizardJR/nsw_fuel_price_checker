67bf30236ceffe6aabaa231582904ed41a8fe8e4
WizardRJ <joe99816@gmail.com>
2024-08-11 02:12:49 +1000

    Cleaned up visualizer

--- src/Visualizer.py
+++ src/Visualizer.py
@@ -1,4 +1,3 @@
-import sqlite3
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
@@ -9,7 +8,7 @@ import json
 from Database import Database
 
 class Visualizer:
-    def __init__(self, configs, fuel_type, start_date = None, end_date = None, station_codes = None, interval = 'D'):
+    def __init__(self, configs, fuel_type, start_date=None, end_date=None, station_codes=None, interval='D'):
         self.db = Database(configs)
         self.fuel_type = fuel_type
         self.start_date = start_date
@@ -18,72 +17,44 @@ class Visualizer:
         self.interval = interval
 
     def plot_prices(self):
-        # Raw data
-        df = self.db.fetch_data(fuel_type, start_date, end_date, [station_codes] if station_codes else None)
+        stations = [self.station_codes] if self.station_codes else None
+        df = self.db.fetch_data(self.fuel_type, self.start_date, self.end_date, stations)
+        avg_df = self.db.fetch_average_price(self.fuel_type, self.start_date, self.end_date, stations, interval=self.interval)
 
-        # Average price data
-        avg_df = self.db.fetch_average_price(fuel_type, start_date, end_date, [station_codes] if station_codes else None, interval=interval)
+        for dataframe in [df, avg_df]:
+            try:
+                dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'], unit='s')
+            except ValueError:
+                dataframe['timestamp'] = pd.to_datetime(dataframe['timestamp'])
+            dataframe['timestamp'] = dataframe['timestamp'].dt.tz_localize('UTC').dt.tz_convert('Australia/Sydney')
 
-        try:
-            avg_df['timestamp'] = pd.to_datetime(avg_df['timestamp'], unit='s')
-        except ValueError:
-            avg_df['timestamp'] = pd.to_datetime(avg_df['timestamp'])
-
-        avg_df['timestamp'] = avg_df['timestamp'].dt.tz_localize('UTC').dt.tz_convert('Australia/Sydney')
         avg_df['date'] = avg_df['timestamp'].dt.date
-
-        try:
-            df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-        except ValueError:
-            df['timestamp'] = pd.to_datetime(df['timestamp'])
-
-        df['timestamp'] = df['timestamp'].dt.tz_localize('UTC').dt.tz_convert('Australia/Sydney')
-
         if avg_df.empty:
             print('No matches')
             return
 
-        daily_avg_df = avg_df.groupby('date').agg(
-            daily_avg_price=('price', 'mean')
-        ).reset_index()
+        daily_avg_df = avg_df.groupby('date').agg(daily_avg_price=('price', 'mean')).reset_index()
         daily_avg_df['date'] = pd.to_datetime(daily_avg_df['date'])
-        # Default size
-        plt.figure(figsize=(14, 8))
 
-        # Titles, labels
-        plt.title(f'Average Fuel Prices for All Stations ({fuel_type})')
+        plt.figure(figsize=(14, 8))
+        plt.title(f'Average Fuel Prices for All Stations ({self.fuel_type})')
         plt.xlabel('Date')
         plt.ylabel('Average Price')
 
-        # x-axis ticks
         x_tick_positions = pd.date_range(start=daily_avg_df['date'].min(), end=daily_avg_df['date'].max())
-        x_tick_labels = x_tick_positions.strftime('%Y-%m-%d')
-        plt.xticks(ticks=x_tick_positions, labels=x_tick_labels, rotation=45)
+        plt.xticks(ticks=x_tick_positions, labels=x_tick_positions.strftime('%Y-%m-%d'), rotation=45)
 
-        # y-axis ticks
-        # min_price = math.floor(df['price'].min())
-        # max_price = math.ceil(df['price'].max())
         min_price = math.floor(daily_avg_df['daily_avg_price'].min()) - 30
         max_price = math.ceil(daily_avg_df['daily_avg_price'].max()) + 30
         min_price = min_price - (min_price % 5)
         max_price = math.ceil(max_price / 5) * 5
-        y_ticks = np.arange(min_price, max_price + 5, 5)
+        y_ticks = np.arange(min_price, max_price, 5)
         plt.yticks(ticks=y_ticks)
         plt.ylim(min_price, max_price)
 
-        # Convert dates for plotting
-        x_daily_avg = mdates.date2num(daily_avg_df['date'])
-        y_daily_avg = daily_avg_df['daily_avg_price']
-
-        x_all = mdates.date2num(df['timestamp'])
-        y_all = df['price']
-
-        # Daily average
-        plt.plot(x_daily_avg, y_daily_avg, color='black', linestyle='--', label='Daily Average')
-        plt.scatter(x_daily_avg, y_daily_avg, color='red', label='Daily Average Points')
-
-        # All datapoints
-        plt.scatter(x_all, y_all, color='blue', alpha=0.05, label='All Data Points')
+        plt.plot(mdates.date2num(daily_avg_df['date']), daily_avg_df['daily_avg_price'], color='black', linestyle='--', label='Daily Average')
+        plt.scatter(mdates.date2num(daily_avg_df['date']), daily_avg_df['daily_avg_price'], color='red', label='Daily Average Points')
+        plt.scatter(mdates.date2num(df['timestamp']), df['price'], color='blue', alpha=0.05, label='All Data Points')
 
         plt.legend()
         plt.tight_layout()
@@ -100,4 +71,3 @@ if __name__ == "__main__":
     end_date = datetime.now().timestamp()
     visualizer = Visualizer(configs, fuel_type, start_date, end_date, station_codes)
     visualizer.plot_prices()
-
