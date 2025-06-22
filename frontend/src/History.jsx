a922c5b16702123bd3e857b5affa81533426bf50
WizardJR <joeqjin@gmail.com>
2025-06-23 00:37:13 +1000

    Implemented historical price graph, interval selector for prices of past n days

---
+++ frontend/src/History.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+const API_URL = import.meta.env.VITE_BACKEND_API_URL;
+import "./History.css"
+import IntervalSelector from "./IntervalSelector";
+import { LineChart, Line, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
+
+const getDateNDaysAgoInclusive = (n) => {
+  const d = new Date();
+  d.setDate(d.getDate() - (n - 1)); //Include first and last day
+  return d.toISOString().slice(0, 10);
+};
+
+function History({fuelType}) {
+  const [data, setData] = useState(null);
+  const end = new Date().toISOString().slice(0, 10);
+  const [interval, setInterval] = useState("30");
+  const [start, setStart] = useState(getDateNDaysAgoInclusive(interval));
+
+  useEffect(() => {
+    setStart(getDateNDaysAgoInclusive(interval));
+  }, [interval]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/average_price_daily/?fuel_type=${fuelType}&start_date=${start}&end_date=${end}`)
+      .then(res => res.json())
+      .then(json => {
+        setData(json ?? "N/A");
+        console.log("Fetched data:", json);
+    });
+
+  }, [fuelType, start]);
+  
+
+  const yMin = data && data.length
+    ? Math.floor(Math.min(...data.map(d => d.avg_price)) / 5) * 5
+    : 0;
+
+  const yMax = data && data.length
+    ? Math.ceil(Math.max(...data.map(d => d.avg_price)) / 5) * 5
+    : 100;
+
+    return (
+    <div className="home-bg">
+        <IntervalSelector value={interval} onChange={setInterval} />
+        <h2 className="graph-title">Price Range ({fuelType})</h2>
+      <div className="history-chart-container" data-testid="history-chart-container">
+        <ResponsiveContainer>
+          <LineChart data={data}>
+            <XAxis dataKey="date" />
+            <YAxis dataKey="avg_price" domain={[yMin, yMax]}/>
+            <Tooltip
+            followCursor={true}
+              formatter={(value) => [`${value.toFixed(2)}`, 'Average Price']}
+              labelFormatter={(label) => `Date: ${label}`}
+            />
+            <Line type="monotone" dataKey="avg_price" stroke="#222" dot />
+          </LineChart>
+        </ResponsiveContainer>
+        </div>
+    </div>
+    );
+}
+
+export default History;
